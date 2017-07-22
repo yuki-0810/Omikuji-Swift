8874a75f2cf8e912489cdd7bffa4582f5f3f3ee2
yuki.abe <kuraea.kura@gmail.com>
2017-07-22 09:38:55 +0900

    Omikuji-Practice

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func getOmikuji(_ sender: Any) {
+        //0 - n
+        // arc4random_uniform(n+1)
+        let results = ["大吉","中吉","吉","凶","大凶"]
+//      let random = arc4random_uniform(3)
+        
+        let random = arc4random_uniform(UInt32(results.count))
+        
+//      self.myLabel.text = String(random)
+        self.myLabel.text = results[Int(random)]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myLabel.layer.borderColor = UIColor.orange.cgColor
+        //myLabel.layer.borderWidth = 5
+        //myLabel.layer.cornerRadius = 50
+        myLabel.layer.masksToBounds = true
+        myLabel.layer.cornerRadius = myLabel.bounds.width / 2
     }
 
     override func didReceiveMemoryWarning() {
